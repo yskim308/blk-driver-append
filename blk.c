47a650032463ced770a62ae93cf9a1289cd5300a
yskim308 <youngseokim7@gmail.com>
2026-05-28 21:21:00 +0900

    init and register ramblk

--- blk.c
+++ blk.c
@@ -1,6 +1,8 @@
 #include "linux/blk_types.h"
+#include "linux/blkdev.h"
 #include "linux/bvec.h"
 #include "linux/highmem-internal.h"
+#include "linux/slab.h"
 #include <linux/blk-mq.h>
 #include <linux/init.h> /* Needed for the macros */
 #include <linux/module.h> /* Needed by all modules */
@@ -13,7 +15,9 @@ struct ramblk_dev {
 	struct gendisk *disk;
 };
 
+const int TOTAL_BYTES = 16 * 1024 * 1024;
 static struct ramblk_dev *ramblk;
+static int ramblk_major;
 
 static bool ramblk_rw_bvec(struct ramblk_dev *ramblk, struct bio *bio)
 {
@@ -47,6 +51,70 @@ static void ramblk_submit_bio(struct bio *bio)
 	} while (bio->bi_iter.bi_size);
 
 	bio_endio(bio);
+}
+
+static const struct block_device_operations ramblk_fops = {
+	.owner = THIS_MODULE,
+	.submit_bio = ramblk_submit_bio,
+};
+
+static int __init ramblk_init(void)
+{
+	struct gendisk *disk;
+	struct queue_limits lim = {
+		.logical_block_size = 512,
+		.physical_block_size = 512,
+		.features = BLK_FEAT_SYNCHRONOUS,
+	};
+	int err;
+
+	ramblk_major = register_blkdev(0, "ramblk");
+	if (ramblk_major < 0) {
+		return ramblk_major;
+	}
+
+	ramblk = kzalloc(sizeof(*ramblk), GFP_KERNEL);
+	if (!ramblk) {
+		unregister_blkdev(ramblk_major, "ramblk");
+		return -ENOMEM;
+	}
+
+	ramblk->data = vmalloc(TOTAL_BYTES);
+	if (!ramblk->data) {
+		err = -ENOMEM;
+		goto out_free_dev;
+	}
+
+	ramblk->disk = blk_alloc_disk(&lim, NUMA_NO_NODE);
+	if (IS_ERR(ramblk->disk)) {
+		err = PTR_ERR(disk);
+		goto out_free_vmalloc;
+	}
+
+	ramblk->disk->major = ramblk_major;
+	ramblk->disk->first_minor = 0;
+	ramblk->disk->minors = 0;
+	ramblk->disk->fops = &ramblk_fops;
+	ramblk->disk->private_data = ramblk;
+	strscpy(disk->disk_name, "ramblk0", DISK_NAME_LEN);
+
+	set_capacity(ramblk->disk, 32768);
+
+	err = add_disk(disk);
+	if (err) {
+		goto out_cleanup_disk;
+	}
+
+	pr_info("ramblk: module loaded\n");
+	return 0;
+out_cleanup_disk:
+	put_disk(ramblk->disk);
+out_free_vmalloc:
+	vfree(ramblk->data);
+out_free_dev:
+	kfree(ramblk);
+	unregister_blkdev(ramblk_major, "ramblk");
+	return err;
 }
 
 module_init(ramblk_init);
